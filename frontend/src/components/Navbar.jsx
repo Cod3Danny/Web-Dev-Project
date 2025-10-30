f93cde2f01024088e385e0fd99f08899cf2fb2b3
Zhi Huang Li <zli8362@bths.edu>
2025-10-29 22:29:37 -0400

    nav responsiveness

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,20 +1,36 @@
-import './Navbar.css'
+import { useState, useEffect } from "react";
+import "./Navbar.css";
+
 const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
         <nav>
+            <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+                <div className={`bar ${menuOpen ? "open" : ""}`}></div>
+                <div className={`bar ${menuOpen ? "open" : ""}`}></div>
+                <div className={`bar ${menuOpen ? "open" : ""}`}></div>
+            </div>
             <div className="logo">Movie Explorer</div>
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="tv">TV</a></li>
-                <li><a href="movies">Movies</a></li>
-                <li><a href="watchlist">Watch List</a></li>
+
+            <ul className={`nav-links ${menuOpen ? "open" : ""}`}>
+                <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+                    <div className={`bar ${menuOpen ? "open" : ""}`}></div>
+                    <div className={`bar ${menuOpen ? "open" : ""}`}></div>
+                    <div className={`bar ${menuOpen ? "open" : ""}`}></div>
+                </div>
+                <li><a className='nav-link' href="/">Home</a></li>
+                <li><a className='nav-link' href="/tv">TV</a></li>
+                <li><a className='nav-link' href="/movies">Movies</a></li>
+                <li><a className='nav-link' href="/watchlist">Watch List</a></li>
             </ul>
+
             <div className="nav-right">
                 <a href="/search" className="icon">🔍</a>
                 <div className="profile">?</div>
             </div>
         </nav>
-    )
-}
+    );
+};
 
-export default Navbar; 
+export default Navbar;
